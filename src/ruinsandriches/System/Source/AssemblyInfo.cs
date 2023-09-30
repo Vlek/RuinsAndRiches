bc320af9b34ffb818c1be601eeab367df55efe6b
vlek <dmccammond25@gmail.com>
2023-09-30 01:49:01 -0700

    Removed unused using.

--- src/ruinsandriches/System/Source/AssemblyInfo.cs
+++ src/ruinsandriches/System/Source/AssemblyInfo.cs
@@ -19,7 +19,6 @@
 ***************************************************************************/
 
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 //
